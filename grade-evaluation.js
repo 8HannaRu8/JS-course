1616e429fa7a4c3dad4be4ad3bdd360197a7da55
Hanna Ru <112951096+8HannaRu8@users.noreply.github.com>
2022-10-28 22:29:52 +0300

    Update grade-evaluation.js

--- grade-evaluation.js
+++ grade-evaluation.js
@@ -1,1 +1,18 @@
+let testScore = 89;
+let missed = false;
+let grade;
 
+if (testScore >= 90) {
+  grade = `A`;
+} else if (testScore >= 80) {
+  grade = `B`;
+} else if (testScore >= 60) {
+  grade = `C`;
+} else {
+  if (missed) {
+    grade = `unevaluated`;
+  } else {
+    grade = `F`;
+  }
+}
+console.log(grade);
