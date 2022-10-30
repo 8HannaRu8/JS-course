a814ac290cbbc696e0d44513261df5e0a17c78a6
Hanna Ru <112951096+8HannaRu8@users.noreply.github.com>
2022-10-30 13:50:47 +0300

    Create ternary-op-soup-order.js

---
+++ ternary-op-soup-order.js
@@ -0,0 +1,10 @@
+let soup = `Chicken Noodle Soup`;
+let isCustomerBanned = false;
+
+let soupAccess = isCustomerBanned
+  ? `Sorry, no soup for you.`
+  : soup
+  ? `Yes, we have a ${soup}.`
+  : `Sorry, no soup today.`;
+
+console.log(soupAccess);
